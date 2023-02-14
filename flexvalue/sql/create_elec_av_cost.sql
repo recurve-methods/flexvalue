2646701b54164f06b801819801de67f9e387c7ee
Matthew Wyatt <matt.wyatt@recurve.com>
2023-02-14 00:50:05 +0000

    Add columns after seeing real dataset

--- flexvalue/sql/create_elec_av_cost.sql
+++ flexvalue/sql/create_elec_av_cost.sql
@@ -1,8 +1,26 @@
 CREATE TABLE elec_av_costs (
+    state VARCHAR,
     utility VARCHAR,
     region VARCHAR,
+    date_time VARCHAR, -- TODO figure this out for the various dbs
     year INTEGER,
+    quarter INTEGER,
+    month INTEGER,
+    hour_of_day INTEGER,
     hour_of_year INTEGER,
+    energy FLOAT,
+    losses FLOAT,
+    ancillary_services FLOAT,
+    capacity FLOAT,
+    transmission FLOAT,
+    distribution FLOAT,
+    cap_and_trade FLOAT,
+    ghg_adder FLOAT,
+    ghg_rebalancing FLOAT,
+    methane_leakage FLOAT,
     total FLOAT,
-    marginal_ghg FLOAT
-);    
+    marginal_ghg FLOAT,
+    ghg_adder_rebalancing FLOAT,
+    load_shape_name VARCHAR,
+    value FLOAT
+);

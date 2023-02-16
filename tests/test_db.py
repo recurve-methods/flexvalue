3470c0d724dd4a1384123e9c29585bea8a88417d
Matthew Wyatt <matt.wyatt@recurve.com>
2023-02-16 19:15:58 +0000

    Add tests for therms profiles, electric avoided costs, gas avoided costs

--- tests/test_db.py
+++ tests/test_db.py
@@ -22,16 +22,34 @@ import pytest
 from flexvalue.db import DBManager
 
 @pytest.fixture
-def dbm_with_project_loaded():
-    dbm = DBManager("")
+def dbm():
+    return DBManager("")
+
+@pytest.fixture
+def dbm_with_project_loaded(dbm):
     dbm.load_project_info_file("example_user_inputs_deer.csv")
     return dbm
 
 @pytest.fixture
-def dbm_with_elec_load_shape_loaded():
-    dbm = DBManager("")
+def dbm_with_elec_load_shape_loaded(dbm):
     # TRUNCATE the table first so we can assert the correct number of rows
     dbm.load_elec_load_shapes_file("ca_hourly_electric_load_shapes.csv", truncate=True)
+    return dbm
+
+@pytest.fixture
+def dbm_with_therms_profiles_loaded(dbm):
+    dbm.load_therms_profiles_file("ca_monthly_therms_load_profiles.csv", truncate=True)
+    return dbm
+
+@pytest.fixture
+def dbm_with_elec_avcosts_loaded(dbm):
+    # NOTE This takes several minutes
+    dbm.load_elec_avoided_costs_file("full_ca_avoided_costs_2020acc.csv", truncate=True)
+    return dbm
+
+@pytest.fixture
+def dbm_with_gas_avcosts_loaded(dbm):
+    dbm.load_gas_avoided_costs_file("full_ca_avoided_costs_2020acc_gas.csv", truncate=True)
     return dbm
 
 # _exec_select_sql returns a list of tuples, so for SELECT COUNT(*) queries
@@ -47,3 +65,16 @@ def test_discount(dbm_with_project_loaded):
 def test_elec_load_shape(dbm_with_elec_load_shape_loaded):
     result = dbm_with_elec_load_shape_loaded._exec_select_sql("SELECT COUNT(*) FROM elec_load_shape;")
     assert result[0][0] == 665760
+
+def test_therms_profiles(dbm_with_therms_profiles_loaded):
+    result = dbm_with_therms_profiles_loaded._exec_select_sql("SELECT COUNT(*) FROM therms_profile;")
+    assert result[0][0] == 144
+
+# NOTE This test takes several minutes to run due to loading the data.
+def test_elec_avoided_costs(dbm_with_elec_avcosts_loaded):
+    result = dbm_with_elec_avcosts_loaded._exec_select_sql("SELECT COUNT(*) FROM elec_av_costs;")
+    assert result[0][0] == 6167040
+
+def test_gas_avoided_costs(dbm_with_gas_avcosts_loaded):
+    result = dbm_with_gas_avcosts_loaded._exec_select_sql("SELECT COUNT(*) FROM gas_av_costs;")
+    assert result[0][0] == 1488

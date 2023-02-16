2b64c3b0a882953b71eafe4cfb783fc1fb14a400
Matthew Wyatt <matt.wyatt@recurve.com>
2023-02-16 00:19:19 +0000

    Add columns to match spec, data

--- flexvalue/sql/create_elec_load_shape.sql
+++ flexvalue/sql/create_elec_load_shape.sql
@@ -1,6 +1,8 @@
 CREATE TABLE elec_load_shape (
+    state VARCHAR,
     utility VARCHAR,
+    region VARCHAR,
     load_shape_name VARCHAR,
     hour_of_year INTEGER,
     value FLOAT
-);    
+);
--- flexvalue/sql/create_therms_profile.sql
+++ flexvalue/sql/create_therms_profile.sql
@@ -2,6 +2,7 @@ CREATE TABLE therms_profile (
     state VARCHAR,
     utility VARCHAR,
     region VARCHAR,
+    quarter VARCHAR,
     month INTEGER,
     profile_name VARCHAR,
     value FLOAT

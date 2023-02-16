8856f8c2d120e0e658b9a74e30a4cc3dcf665d8a
Matthew Wyatt <matt.wyatt@recurve.com>
2023-02-16 00:23:01 +0000

    Enable loading therms profiles data

--- flexvalue/db.py
+++ flexvalue/db.py
@@ -181,6 +181,40 @@ class DBManager:
             result = self._exec_insert_sql(sql)
             print(f"loaded {result.rowcount} rows")
 
+    def load_therms_profiles_file(self, therms_profiles_path: str):
+        self._prepare_table('therms_profile', 'flexvalue/sql/create_therms_profile.sql')
+        with open(therms_profiles_path, newline='') as f:
+            # 4096 was determined empirically; I don't recommend reading less
+            # than this, since there can be so many columns
+            has_header = csv.Sniffer().has_header(f.read(4096))
+            if not has_header:
+                raise ValueError(f"The therms profiles file you provided, {therms_profiles_path}, \
+                                 doesn't seem to have a header row. Please provide a header row \
+                                 containing the profile names.")
+            f.seek(0)
+            csv_reader = csv.reader(f)
+            rows = []
+            # TODO do we need to chunk this?
+            for row in csv_reader:
+                rows.append(row)
+            insert_text = self._file_to_string("flexvalue/templates/load_therms_profiles.sql")
+            # TODO can we clean this up?
+            num_columns = len(rows[0])
+            buffer = []
+            for col in range(5,num_columns):
+                for row in range(1,len(rows)):
+                    buffer.append({
+                        'state': rows[row][0],
+                        'utility': rows[row][1],
+                        'region': rows[row][2],
+                        'quarter': rows[row][3],
+                        'month': rows[row][4],
+                        'profile_name': rows[0][col],
+                        'value': rows[row][col],
+                    })
+            with self.engine.begin() as conn:
+                conn.execute(text(insert_text), buffer)
+
     def _load_csv_file(self, csv_file_path:str, table_name: str, fieldnames, load_sql_file_path:str):
         """ Loads the table_name table, Since some of the input data can be over a gibibyte,
         the load reads in chunks of data and inserts them sequentially. The chunk size is
@@ -210,9 +244,6 @@ class DBManager:
                 print(f"In finally, inserting {len(buffer)} rows")
                 with self.engine.begin() as conn:
                     conn.execute(text(insert_text), buffer)
-
-    def load_therms_profiles_file(self, therms_profiles_path: str):
-        self._prepare_table('therms_profile', 'flexvalue/sql/create_therms_profile.sql')
 
     def load_elec_avoided_costs_file(self, elec_av_costs_path: str):
         self._prepare_table('elec_av_costs', 'flexvalue/sql/create_elec_av_cost.sql')

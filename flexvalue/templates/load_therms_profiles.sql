53ff68a3f1f4b8039469122e7b2a8bc85000687e
Matthew Wyatt <matt.wyatt@recurve.com>
2023-02-16 00:18:56 +0000

    Commit initial version of sql to load therms profiles

---
+++ flexvalue/templates/load_therms_profiles.sql
@@ -0,0 +1,12 @@
+INSERT INTO therms_profile (
+    state,
+    utility,
+    region,
+    quarter,
+    month,
+    profile_name,
+    value
+)
+VALUES (
+    :state, :utility, :region, :quarter, :month, :profile_name, :value
+)

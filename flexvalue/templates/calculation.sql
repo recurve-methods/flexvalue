48dcf5875d01eb19b37717b578c0664693bf4f83
Matthew Wyatt <matt.wyatt@recurve.com>
2023-02-21 00:20:56 +0000

    Add first_year_net_mwh_savings

--- flexvalue/templates/calculation.sql
+++ flexvalue/templates/calculation.sql
@@ -1,9 +1,10 @@
-SELECT p.project_id
+SELECT p.project_id as project_id
 , (p.admin_cost + (1 - p.ntg) * p.incentive_cost + p.ntg * p.measure_cost) / 1 + (p.discount_rate / 4) as trc_costs
 , (p.admin_cost + p.incentive_cost) / (1 + (p.discount_rate / 4)) as pac_costs
 , p.units * p.mwh_savings * p.ntg * sum(els.value) as elec_savings
 -- elec benefits: discount_q * (p.units * p.ntg * the electric_av_costs.total for the (utility, region, years, hour of year) (all of which are in or derived from project_info) * the savings for the hour (from the load shape, and the project_info.mwh_savings) * the discount for the quarter (from the discount table) as elec_benefits
 , (p.units * p.ntg * sum(eac.total) * sum(els.value) * d.discount) as elec_benefits
+, p.units * p.mwh_savings * p.ntg * sum(els.value) as first_year_net_mwh_savings
 FROM project_info p
 -- TODO make JOIN ON region dynamic for when the eac data has region? Or will it always?
 JOIN elec_av_costs eac ON p.state = eac.state AND p.utility = eac.utility AND p.region = eac.region

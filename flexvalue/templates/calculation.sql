b5203367daa16eb4eb530f7ed852024728d4ec82
Matthew Wyatt <matt.wyatt@recurve.com>
2023-03-01 17:57:26 +0000

    Fix discount multiplication; reorder join to try to hit index more

--- flexvalue/templates/calculation.sql
+++ flexvalue/templates/calculation.sql
@@ -3,17 +3,17 @@
 -- TODO make JOIN ON region dynamic for when the therms profiles have region?
 -- TODO make GROUP BY dynamic (aggregation columns)
 SELECT p.project_id as project_id
-, (p.units * p.ntg * sum(eac.total * els.value) * d.discount) AS elec_benefits
-, (p.units * p.ntg * sum(gac.total * tp.value) * d.discount) as gas_benefits
-, ((p.units * p.ntg * sum(eac.total * els.value) * d.discount) + (p.units * p.ntg * sum(gac.total * tp.value) * d.discount)) as total_benefits
+, (p.units * p.ntg * sum(eac.total * els.value * d.discount)) AS elec_benefits
+, (p.units * p.ntg * sum(gac.total * tp.value * d.discount)) as gas_benefits
+, ((p.units * p.ntg * sum(eac.total * els.value * d.discount)) + (p.units * p.ntg * sum(gac.total * tp.value * d.discount))) as total_benefits
 , p.units * p.mwh_savings * p.ntg * sum(els.value) as first_year_net_mwh_savings
 , p.units * p.mwh_savings * p.ntg * sum(els.value) * p.eul as project_lifecycle_elec_savings
 , p.units * p.therms_savings * ntg * sum(tp.value) as first_year_net_therms_savings
 , p.units * p.therms_savings * ntg * sum(tp.value) * p.eul as project_lifecycle_therms_savings
 , (p.admin_cost + (1 - p.ntg) * p.incentive_cost + p.ntg * p.measure_cost) / 1 + (p.discount_rate / 4) as trc_costs
 , (p.admin_cost + p.incentive_cost) / (1 + (p.discount_rate / 4)) as pac_costs
-, ((p.units * p.ntg * sum(eac.total * els.value) * d.discount) + (p.units * p.ntg * sum(gac.total * tp.value) * d.discount)) / (p.admin_cost + (1 - p.ntg) * p.incentive_cost + p.ntg * p.measure_cost) / 1 + (p.discount_rate / 4) AS trc_ratio
-, ((p.units * p.ntg * sum(eac.total * els.value) * d.discount) + (p.units * p.ntg * sum(gac.total * tp.value) * d.discount)) / (p.admin_cost + p.incentive_cost) / (1 + (p.discount_rate / 4)) AS pac_ratio
+, ((p.units * p.ntg * sum(eac.total * els.value * d.discount)) + (p.units * p.ntg * sum(gac.total * tp.value * d.discount))) / (p.admin_cost + (1 - p.ntg) * p.incentive_cost + p.ntg * p.measure_cost) / 1 + (p.discount_rate / 4) AS trc_ratio
+, ((p.units * p.ntg * sum(eac.total * els.value * d.discount)) + (p.units * p.ntg * sum(gac.total * tp.value * d.discount))) / (p.admin_cost + p.incentive_cost) / (1 + (p.discount_rate / 4)) AS pac_ratio
 , (p.units * p.ntg * sum(eac.marginal_ghg * els.value)) as elec_avoided_ghg
 , (p.units * p.ntg * sum(gac.marginal_ghg * tp.value)) as gas_avoided_ghg
 , (p.units * p.ntg * sum(eac.marginal_ghg * els.value)) + (p.units * p.ntg * sum(gac.marginal_ghg * tp.value)) as total_avoided_ghg
@@ -21,7 +21,7 @@ FROM project_info p
 JOIN discount d on p.project_id = d.project_id
 JOIN gas_av_costs gac on p.state = gac.state AND p.utility = gac.utility
 JOIN therms_profile tp on p.state = tp.state AND p.utility = tp.utility AND tp.profile_name = p.therms_profile
-JOIN elec_av_costs eac ON p.state = eac.state AND p.utility = eac.utility AND p.region = eac.region
+JOIN elec_av_costs eac ON p.region = eac.region AND p.utility = eac.utility AND p.state = eac.state
 JOIN elec_load_shape els ON p.elec_load_shape = els.load_shape_name AND p.state = els.state AND p.utility = els.utility
 WHERE ((eac.year = p.start_year and eac.quarter >= p.start_quarter) OR (eac.year between p.start_year + 1 AND p.start_year + p.eul - 1) OR (eac.year = p.start_year + p.eul and eac.quarter < p.start_quarter))
 AND eac.hour_of_year = els.hour_of_year AND eac.state = els.state AND eac.utility = els.utility

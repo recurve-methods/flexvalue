0428fc4e4a246ed834dcc8a67e438c356d54a173
Matthew Wyatt <matt.wyatt@recurve.com>
2023-03-02 23:12:11 +0000

    Load hoy_util_st column

--- flexvalue/templates/load_elec_load_shape.sql
+++ flexvalue/templates/load_elec_load_shape.sql
@@ -6,7 +6,8 @@ INSERT INTO elec_load_shape (
     month,
     hour_of_day,
     hour_of_year,
+    hoy_util_st,
     load_shape_name,
     value
 )
-VALUES (:state, :utility, :region, :quarter, :month, :hour_of_day, :hour_of_year, :load_shape_name, :value)
+VALUES (:state, :utility, :region, :quarter, :month, :hour_of_day, :hour_of_year, :hoy_util_st, :load_shape_name, :value)

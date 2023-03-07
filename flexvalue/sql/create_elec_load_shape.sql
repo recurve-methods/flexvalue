61a1e27278d455c724b1de1048e69838a1ad7dcd
Matthew Wyatt <matt.wyatt@recurve.com>
2023-03-07 02:30:44 +0000

    Remove hoy_util_st, add timesamp

--- flexvalue/sql/create_elec_load_shape.sql
+++ flexvalue/sql/create_elec_load_shape.sql
@@ -1,5 +1,6 @@
 CREATE TABLE elec_load_shape (
     pk SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP,
     state TEXT,
     utility TEXT,
     region TEXT,
@@ -7,7 +8,7 @@ CREATE TABLE elec_load_shape (
     month INTEGER,
     hour_of_day INTEGER,
     hour_of_year INTEGER,
-    hoy_util_st TEXT,
+    --hoy_util_st TEXT,
     load_shape_name TEXT,
     value FLOAT
 );
--- flexvalue/templates/load_elec_load_shape.sql
+++ flexvalue/templates/load_elec_load_shape.sql
@@ -1,4 +1,5 @@
 INSERT INTO elec_load_shape (
+    timestamp,
     state,
     utility,
     region,
@@ -6,8 +7,7 @@ INSERT INTO elec_load_shape (
     month,
     hour_of_day,
     hour_of_year,
-    hoy_util_st,
     load_shape_name,
     value
 )
-VALUES (:state, :utility, :region, :quarter, :month, :hour_of_day, :hour_of_year, :hoy_util_st, :load_shape_name, :value)
+VALUES (:timestamp, :state, :utility, :region, :quarter, :month, :hour_of_day, :hour_of_year, :load_shape_name, :value)

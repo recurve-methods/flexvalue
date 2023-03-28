b4b4011a55ac38bc4ea35638ccff4db19280115e
Matthew Wyatt <matt.wyatt@recurve.com>
2023-03-28 22:13:10 +0000

    Set default values, add new fields

--- flexvalue/config.py
+++ flexvalue/config.py
@@ -1,5 +1,8 @@
-from dataclasses import dataclass
 import toml
+
+from dataclasses import dataclass, field
+from typing import List
+
 
 class FLEXValueException(Exception):
     pass
@@ -7,25 +10,34 @@ class FLEXValueException(Exception):
 @dataclass
 class FLEXValueConfig:
     database_type: str
-    host: str
-    port: int
-    user: str
-    password: str
-    database: str
-    project: str
-    dataset: str
-    elec_load_shape: str
-    elec_av_costs: str
-    therms_profiles: str
-    gas_av_costs: str
-    project_info: str
-    elec_av_cost_table: str
-    elec_load_shape_table: str
-    therms_profiles_table: str
-    gas_av_cost_table: str
-    project_info_table: str
-    output_file: str
-    output_table: str
+    host: str = None
+    port: int = None
+    user: str = None
+    password: str = None
+    database: str = None
+    project: str = None
+    dataset: str = None
+    elec_load_shape_file: str = None
+    elec_av_costs_file: str = None
+    therms_profiles_file: str = None
+    gas_av_costs_file: str = None
+    project_info_file: str = None
+    elec_av_costs_table: str = None
+    elec_load_shape_table: str = None
+    therms_profiles_table: str = None
+    gas_av_costs_table: str = None
+    project_info_table: str = None
+    output_file: str = None
+    output_table: str = None
+    aggregation_columns: List[str] = field(default_factory=list)
+    process_elec_load_shape: bool = False
+    process_elec_av_costs: bool = False
+    process_therms_profiles: bool = False
+    process_gas_av_costs: bool = False
+    reset_elec_load_shape: bool = False
+    reset_elec_av_costs: bool = False
+    reset_therms_profiles: bool = False
+    reset_gas_av_costs: bool = False
 
     @staticmethod
     def from_file(config_file):
@@ -33,6 +45,7 @@ class FLEXValueConfig:
         print(f"config data = {data}")
         db = data.get('database', dict())
         run = data.get('run', dict())
+        aggregation_columns = ",".split(run.get("aggregation_columns", []))
         return FLEXValueConfig(
             database_type=db.get('database_type', None),
             host=db.get('host', None),
@@ -42,19 +55,25 @@ class FLEXValueConfig:
             database=db.get('database', None),
             project=db.get('project', None),
             dataset=db.get('dataset', None),
-            elec_av_cost_table=db.get("elec_av_cost_table", None),
+            elec_av_costs_table=db.get("elec_av_costs_table", None),
             elec_load_shape_table=db.get("elec_load_shape_table", None),
             therms_profiles_table=db.get("therms_profiles_table", None),
-            gas_av_cost_table=db.get("gas_av_cost_table", None),
+            gas_av_costs_table=db.get("gas_av_costs_table", None),
             project_info_table=db.get("project_info_table", None),
-            elec_load_shape=run.get('elec_load_shape', None),
-            elec_av_costs=run.get('elec_av_costs', None),
-            therms_profiles=run.get('therms_profiles', None),
-            gas_av_costs=run.get('gas_av_costs', None),
-            project_info=run.get('project_info', None),
+            elec_load_shape_file=run.get('elec_load_shape', None),
+            elec_av_costs_file=run.get('elec_av_costs', None),
+            therms_profiles_file=run.get('therms_profiles', None),
+            gas_av_costs_file=run.get('gas_av_costs', None),
+            project_info_file=run.get('project_info', None),
             output_file=run.get('output_file', None),
-            output_table=run.get('output_table', None)
+            output_table=run.get('output_table', None),
+            aggregation_columns=aggregation_columns,
+            reset_elec_load_shape=run.get("reset_elec_load_shape", None),
+            reset_elec_av_costs=run.get("reset_elec_av_costs", None),
+            reset_gas_av_costs=run.get("reset_gas_av_costs", None),
+            reset_therms_profiles=run.get("reset_therms_profiles", None)
         )
+
 
     def use_specified_db(self):
         """ If the user has specified any of the database fields,
@@ -79,23 +98,43 @@ class FLEXValueConfig:
             return "FLOAT"
 
     def elec_aggregation_columns(self):
-        return [
-        {
-            "prefix": "pcwdea",
-            "columns":["hour_of_year", "year", "eul", "utility", "region", "month", "quarter", "discount", "hour_of_day", "timestamp"]
-        }, {
-            "prefix": "elec_load_shape",
-            "columns": ["load_shape_name"]
+        prefix_map = {
+            "hour_of_year": "pcwdea",
+            "year": "pcwdea",
+            "eul": "pcwdea",
+            "utility": "pcwdea",
+            "region": "pcwdea",
+            "month": "pcwdea",
+            "quarter": "pcwdea",
+            "discount": "pcwdea",
+            "hour_of_day": "pcwdea",
+            "timestamp": "pcwdea",
+            "load_shape_name": "elec_load_shape"
         }
-    ]
+        columns = []
+        for col in self.aggregation_columns:
+            try: 
+                columns.append(f"{prefix_map[col]}.{col}")
+            except KeyError:
+                pass
+        return ", ".join(columns)
 
     def gas_aggregation_columns(self):
-        return [
-        {
-            "prefix": "pcwdga",
-            "columns":["year", "eul", "utility", "region", "month", "quarter", "discount", "timestamp"]
-        }, {
-            "prefix": "therms_profile",
-            "columns": ["profile_name"]
+        prefix_map = {
+            "year": "pcwdga",
+            "eul": "pcwdga",
+            "utility": "pcwdga",
+            "region": "pcwdga",
+            "month": "pcwdga",
+            "quarter": "pcwdga",
+            "discount": "pcwdga",
+            "timestamp": "pcwdga",
+            "profile_name": "therms_profile"
         }
-    ]
+        columns = []
+        for col in self.aggregation_columns:
+            try: 
+                columns.append(f"{prefix_map[col]}.{col}")
+            except KeyError:
+                pass
+        return ", ".join(columns)

6b883c8cf3a8c997668f2509429639e0d92bf979
Matthew Wyatt <matt.wyatt@recurve.com>
2023-04-20 01:19:47 +0000

    Initial commit

---
+++ flexvalue/templates/bq_create_therms_profile.sql
@@ -0,0 +1,9 @@
+CREATE TABLE {{ dataset }}.therms_profile (
+    state TEXT,
+    utility TEXT,
+    region TEXT,
+    quarter INTEGER,
+    month INTEGER,
+    profile_name TEXT,
+    value FLOAT64
+);

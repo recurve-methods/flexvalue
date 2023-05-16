59002c55e4c11fac5160d7a07134d214c4c2243c
Matthew Wyatt <matt.wyatt@recurve.com>
2023-05-16 15:22:28 +0000

    Add import to init file

--- flexvalue/__init__.py
+++ flexvalue/__init__.py
@@ -19,7 +19,7 @@
 """
 
 # import logging
-
+import flexvalue.flexvalue
 # from .__version__ import __title__, __description__, __url__, __version__
 # from .__version__ import __author__, __author_email__, __license__
 # from .__version__ import __copyright__

e51a2165c3a647718d3ac5cc27bcd58fa611e3d9
Matthew Wyatt <matt.wyatt@recurve.com>
2023-05-26 18:44:21 +0000

    Change loading metered load shapes to only those found in the project_info file

--- flexvalue/templates/bq_populate_metered_load_shape.sql
+++ flexvalue/templates/bq_populate_metered_load_shape.sql
@@ -1,9 +1,16 @@
-FOR load_shape IN (select column_name from `{{ project }}.{{ dataset }}`.INFORMATION_SCHEMA.COLUMNS
-WHERE table_name="{{ metered_load_shape_table }}"
-AND column_name NOT IN ("state", "utility", "region", "quarter", "month", "hour_of_day", "hour_of_year"))
+FOR row in (
+  select distinct load_shape, utility
+  FROM `{{ project }}.{{ dataset }}.{{ project_info_table }}`
+  where upper(load_shape) in (
+    select upper(column_name) from `{{ project }}.{{ dataset }}`.INFORMATION_SCHEMA.COLUMNS
+    WHERE table_name="{{ metered_load_shape_table }}"
+    AND column_name NOT IN ("state", "utility", "region", "quarter", "month", "hour_of_day", "hour_of_year")
+  )
+)
+
 DO
   EXECUTE IMMEDIATE format(
   """INSERT INTO {{ dataset }}.elec_load_shape (utility, hour_of_year, load_shape_name, value)
-  SELECT UPPER("{{ utility }}"), hour_of_year, UPPER("%s"), %s
-  FROM {{project}}.{{dataset}}.{{metered_load_shape_table}};""", load_shape.column_name, load_shape.column_name);
+  SELECT UPPER("%s"), hour_of_year, UPPER("%s"), %s
+  FROM {{ project }}.{{ dataset }}.{{ metered_load_shape_table }};""", row.utility, row.load_shape, row.load_shape);
 END FOR;

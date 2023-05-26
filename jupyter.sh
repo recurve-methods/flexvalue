90e224d8dbff8dd9d584d1ad704900aa2e9ba864
Matthew Wyatt <matt.wyatt@recurve.com>
2023-05-26 02:04:11 +0000

    Remove unused file

--- jupyter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# first argument is the port to run on 
-# if not specified, select a random port to run on
-PORT=${1:-`python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'`}
-HOST_PORT_JUPYTER=$PORT docker-compose up jupyter 
-

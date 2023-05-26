3d5dc51ba8d03f7901fb9c4c33470d9fabd11ded
Matthew Wyatt <matt.wyatt@recurve.com>
2023-05-26 23:03:07 +0000

    Delete unused file

--- flexvalue/templates/metered_load_shapes_query.sql
+++
@@ -1,3 +0,0 @@
-select column_name from `{{ project }}.{{ dataset }}`.INFORMATION_SCHEMA.COLUMNS
-WHERE table_name="{{ metered_load_shape_table }}"
-AND column_name NOT IN ("state", "utility", "region", "quarter", "month", "hour_of_day", "hour_of_year")

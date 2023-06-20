10920d698e39b3177f46fda1a231295ae7e6407f
Matthew Wyatt <matt.wyatt@recurve.com>
2023-06-20 22:59:59 +0000

    Cast eul to int to support DATETIME

--- flexvalue/templates/calculation.sql
+++ flexvalue/templates/calculation.sql
@@ -33,7 +33,7 @@ project_costs_with_discounted_elec_av AS (
             AND elec_av_costs.datetime < make_timestamp(project_costs.start_year, (project_costs.start_quarter - 1) * 3 + 1, 1, 0, 0, 0) + make_interval(project_costs.eul)
             {% else -%}
             AND elec_av_costs.datetime >= CAST(DATE(project_costs.start_year, (project_costs.start_quarter - 1) * 3 + 1, 1) AS DATETIME)
-            AND elec_av_costs.datetime < CAST(DATE(project_costs.start_year + project_costs.eul, (project_costs.start_quarter - 1) * 3 + 1, 1) AS DATETIME)
+            AND elec_av_costs.datetime < CAST(DATE(CAST(project_costs.start_year + project_costs.eul AS INT), (project_costs.start_quarter - 1) * 3 + 1, 1) AS DATETIME)
             {% endif -%}
 ),
 elec_calculations AS (
@@ -92,7 +92,7 @@ elec_calculations AS (
             AND gas_av_costs.datetime < make_timestamp(project_costs.start_year, (project_costs.start_quarter - 1) * 3 + 1, 1, 0, 0, 0) + make_interval(project_costs.eul)
             {% else -%}
             AND gas_av_costs.datetime >= CAST(DATE(project_costs.start_year, (project_costs.start_quarter - 1) * 3 + 1, 1) AS DATETIME)
-            AND gas_av_costs.datetime < CAST(DATE(project_costs.start_year + project_costs.eul, (project_costs.start_quarter - 1) * 3 + 1, 1) AS DATETIME)
+            AND gas_av_costs.datetime < CAST(DATE(CAST(project_costs.start_year + project_costs.eul AS INT), (project_costs.start_quarter - 1) * 3 + 1, 1) AS DATETIME)
             {% endif -%}
 ),
 gas_calculations AS (
